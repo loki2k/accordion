c608ca706d659128a69721e94b99811a4f6d3176
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:21:31 -0800

    Updates

---
+++ precache-manifest.e36b5e3e406dfe38f206465a4402d133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5285948a3da892ae406782a7f53c9ffc",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "b387cccc708caa9db22e",
+    "url": "/accordion/static/css/main.9280bcb1.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b387cccc708caa9db22e",
+    "url": "/accordion/static/js/main.f1102d61.chunk.js"
+  },
+  {
+    "revision": "8bf3a778a198a7a25ab9",
+    "url": "/accordion/static/js/runtime-main.75639503.js"
+  }
+]);
